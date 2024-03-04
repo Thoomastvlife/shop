7b00d7ea719df4edf3a1ea37ce4ba2c49283c6fb
Thoomastvlife <153334866+Thoomastvlife@users.noreply.github.com>
2024-03-04 09:03:38 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>
+<!--
+	Eventually by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>YT集團商城</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="icon" href="./favicon.ico">
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<h1>歡迎光臨</h1>
+				<p>製作團隊<a href="http://mctw.net">主頁</a></p>
+			</header>
+		    <script src="https://js.hcaptcha.com/1/api.js" async defer></script>
+
+<form>
+    <div class="h-captcha" data-sitekey="d6ae0d37-804f-465a-bb3b-a66c5f901722"></div>
+</form>
+
+
+		<!-- Signup Form -->
+			<form id="signup-form" method="post" action="#">
+				<input type="email" name="email" id="email" placeholder="Email Address" />
+				<input type="submit" value="Sign Up" />
+			</form>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<ul class="icons">
+					<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+					<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+					<li><a href="#" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+					<li><a href="mailto:shop@mctw.net" class="icon fa-envelope"></a><span class="label"></span></a></li>
+				</ul>
+				<ul class="copyright">
+					<li>&copy; YTgroupmall</li><li>關於: <a href="http://mctw.net">主頁</a></li>
+				</ul>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
